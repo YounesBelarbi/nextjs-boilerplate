e04b82bb57e5e04c376839a0d598231df8791980
younesbelarbi <ybelarbi@outlook.com>
2024-12-22 20:24:35 +0100

    chore: setup env type safe

---
+++ src/lib/env.ts
@@ -0,0 +1,41 @@
+// import { createEnv } from "@t3-oss/env-nextjs";
+// import { z } from "zod";
+
+// export const env = createEnv({
+//   server: {
+//     SUPABASE_URL: z.string().url(),
+//     SUPABASE_ANON_KEY: z.string().min(1),
+//   },
+//   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+//   // runtimeEnv: {
+//   //   DATABASE_URL: process.env.DATABASE_URL,
+//   //   OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+//   // },
+//   // For Next.js >= 13.4.4, you can just reference process.env:
+//   experimental__runtimeEnv: process.env,
+// });
+
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    // SUPABASE_URL: z.string().url(),
+    // SUPABASE_ANON_KEY: z.string().min(1),
+  },
+  client: {
+    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
+  },
+  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+  // runtimeEnv: {
+  // SUPABASE_URL: process.env.DATABASE_URL,
+  // OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+  // NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+  // },
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+});
